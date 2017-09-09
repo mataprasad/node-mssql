73cd947dc094bcbe266b9c8bc658db6d8b9825fb
mata prasad chauhan <mataprasad045@gmail.com>
2017-09-09 13:21:57 +0530

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require("express");
+const sql = require('mssql')
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+
+// Enable tcp protocol for sql server, set the sttaic port to 1433 for all ips, restart the server
+
+//const DbConnectionString = 'mssql://{user-name}:{password}@{db-server-ip}:{sql-server-port}/{db-name}';
+const DbConnectionString = 'mssql://sa:sequel@192.168.1.103:1433/DbScott';
+
+sql.on('error', err => {
+    console.dir(err);
+    sql.close();
+});
+
+app.get("/", function(req, res) {
+    sql.connect(DbConnectionString).then(pool => {
+        return pool.request()
+            .query('select * from emp;');
+    }).then(result => {
+        res.render("data", { model: result.recordset });
+    }).catch(err => {
+        console.dir(err);
+    });
+});
+
+app.listen("4501", function() {
+    console.log("node server listening at port : 4501");
+});
